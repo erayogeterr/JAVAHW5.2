2937b1416546300a38ed554af44c205179f30fc2
Eray Ögeter <erayogeterr@gmail.com>
2022-11-16 12:15:47 +0300

    some changes exception layer.

--- src/main/java/com/kodlama/io/SpringHW/exception/CannotRepeatNameException.java
+++ src/main/java/com/kodlama/io/SpringHW/exception/CannotRepeatNameException.java
@@ -3,7 +3,7 @@ package com.kodlama.io.SpringHW.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class CannotRepeatNameException extends Exception {
 
 	private static final long serialVersionUID = 1L;
